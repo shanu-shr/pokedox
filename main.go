e4100a4a4a363776d9080b0d81e5fd3e02c746c3
abc <a>
2025-10-25 17:38:18 +0530

    add exit and help command

--- main.go
+++ main.go
@@ -7,16 +7,64 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var supportedCommands map[string]cliCommand
+
+func registerCommands() {
+	supportedCommands = make(map[string]cliCommand)
+
+	supportedCommands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the pokedox",
+		callback:    commandExit,
+	}
+
+	supportedCommands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    helpHandler,
+	}
+}
+
 func main() {
+
+	registerCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedox > ")
 		if scanner.Scan() {
 			words := cleanInput(scanner.Text())
-			fmt.Println("Your command was:", words[0])
+
+			command, ok := supportedCommands[words[0]]
+			if ok {
+				command.callback()
+			} else {
+				fmt.Println("Unknown command")
+			}
 		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func helpHandler() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+
+	for _, item := range supportedCommands {
+		fmt.Println(item.description)
+	}
+	return nil
 }
 
 func cleanInput(text string) []string {
